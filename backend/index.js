7da1140e794d73bfce2e3a2d7fc8bea85a474a45
meironY2008 <67857621+meironY2008@users.noreply.github.com>
2020-09-11 11:33:07 +0300

    get post delete put

--- backend/index.js
+++ backend/index.js
@@ -25,53 +25,147 @@ mysqlCon.connect(err => {
 
 app.get('/songs', (req, res) => {
     mysqlCon.query('SELECT * FROM songs;', (error, results, fields) => {
-        if (error) {
-            res.send(error.message);
-            throw error;
-        };
-        res.send(results);
+        error && res.send(error.message)
+        return res.send(results);
+      });
+});
+app.get('/top/songs', (req, res) => {
+    mysqlCon.query('SELECT * FROM songs LIMIT 10;', (error, results, fields) => {
+        error && res.send(error.message)
+        return res.send(results);
+      });
+});
+app.get('/top/albums', (req, res) => {
+    mysqlCon.query('SELECT * FROM albums LIMIT 10;', (error, results, fields) => {
+        error && res.send(error.message)
+        return res.send(results);
+      });
+});
+app.get('/top/artists', (req, res) => {
+    mysqlCon.query('SELECT * FROM artists LIMIT 10;', (error, results, fields) => {
+        error && res.send(error.message)
+        return res.send(results);
       });
 });
 
-
+/* get song from sql by id or title */
 app.get('/songs/:idOrTitle', async (req, res) =>{
     mysqlCon.query('SELECT * FROM songs WHERE id = ? OR title LIKE ?',[req.params.idOrTitle,`${req.params.idOrTitle}`], (error, results, fields) => {
-        if (error) {
-            res.send(error.message);
-            throw error;
-        };
-        res.send(results);
+        error && res.send(error.message)
+        return res.send(results);
       });
 });
 
-app.post('/song', async (req, res) =>{
+/* get album from sql by id or name */
+app.get('/albums/:idOrname', async (req, res) =>{
+    mysqlCon.query('SELECT * FROM albums WHERE id = ? OR Name LIKE ?',[req.params.idOrname,`${req.params.idOrname}`], (error, results, fields) => {
+        error && res.send(error.message)
+        return res.send(results);
+      });
+});
+
+
+app.get('/artists/:idOrname', async (req, res) =>{
+    mysqlCon.query('SELECT * FROM artists WHERE id = ? OR Name LIKE ?',[req.params.idOrname,`${req.params.idOrname}`], (error, results, fields) => {
+        error && res.send(error.message)
+        return res.send(results);
+      });
+});
+app.get('/playlists/:idOrname', async (req, res) =>{
+    mysqlCon.query('SELECT * FROM playlists WHERE id = ? OR name LIKE ?',[req.params.idOrname,`${req.params.idOrname}`], (error, results, fields) => {
+        error && res.send(error.message)
+        return res.send(results);
+      });
+});
+
+// ----
+
+app.post('/songs', async (req, res) =>{
     mysqlCon.query('INSERT INTO songs SET ?',req.body, (error, results, fields) => {
-        if (error) {
-            res.send(error.message);
-            throw error;
-        };
-        res.send(results);
+        error && res.send(error.message)
+        return res.send(results);
       });
 });
 
-app.put('/song', async (req, res) =>{
-    mysqlCon.query('UPDATE songs SET song_name = ?, artist_id = ?, length = ? WHERE song_id = ?',
-    [req.body.song_name, req.body.artist_id, req.body.length, req.body.song_id], (error, results, fields) => {
-        if (error) {
-            res.send(error.message);
-            throw error;
-        };
-        res.send(results);
+app.post('/playlists', async (req, res) =>{
+    mysqlCon.query('INSERT INTO playlists SET ?',req.body, (error, results, fields) => {
+        error && res.send(error.message)
+        return res.send(results);
+      });
+});
+app.post('/artists', async (req, res) =>{
+    mysqlCon.query('INSERT INTO artists SET ?',req.body, (error, results, fields) => {
+        error && res.send(error.message)
+        return res.send(results);
+      });
+});
+app.post('/albums', async (req, res) =>{
+    mysqlCon.query('INSERT INTO albums SET ?',req.body, (error, results, fields) => {
+        error && res.send(error.message)
+        return res.send(results);
       });
 });
 
-app.delete('/song/:id', async (req, res) =>{
-    mysqlCon.query('DELETE FROM songs WHERE song_id = ?',[req.params.id], (error, results, fields) => {
-        if (error) {
-            res.send(error.message);
-            throw error;
-        };
-        res.send(results);
+// ---
+
+app.put('/songs/:id', (req, res) =>{
+    mysqlCon.query(`UPDATE songs SET ? WHERE id = ${req.params.id}`, req.body, (error, results) => {
+        error
+        ? res.send(error.message)
+        : res.send(results)
+    });
+});
+
+app.put('/playlists/:id', (req, res) =>{
+    mysqlCon.query(`UPDATE playlists SET ? WHERE id = ${req.params.id}`, req.body, (error, results) => {
+        error
+        ? res.send(error.message)
+        : res.send(results)
+    });
+});
+
+app.put('/artists/:id', (req, res) =>{
+    mysqlCon.query(`UPDATE artists SET ? WHERE id = ${req.params.id}`, req.body, (error, results) => {
+        error
+        ? res.send(error.message)
+        : res.send(results)
+    });
+});
+
+app.put('/albums/:id', (req, res) =>{
+    mysqlCon.query(`UPDATE albums SET ? WHERE id = ${req.params.id}`, req.body, (error, results) => {
+        error
+        ? res.send(error.message)
+        : res.send(results)
+    });
+});
+
+
+
+// ----
+
+app.delete('/songs/:id', async (req, res) =>{
+    mysqlCon.query('DELETE FROM songs WHERE id = ?',[req.params.id], (error, results, fields) => {
+        error && res.send(error.message)
+        return res.send(results);
+      });
+});
+app.delete('/albums/:id', async (req, res) =>{
+    mysqlCon.query('DELETE FROM albums WHERE id = ?',[req.params.id], (error, results, fields) => {
+        error && res.send(error.message)
+        return res.send(results);
+      });
+});
+app.delete('/artists/:id', async (req, res) =>{
+    mysqlCon.query('DELETE FROM artists WHERE id = ?',[req.params.id], (error, results, fields) => {
+        error && res.send(error.message)
+        return res.send(results);
+      });
+});
+app.delete('/playlists/:id', async (req, res) =>{
+    mysqlCon.query('DELETE FROM playlists WHERE id = ?',[req.params.id], (error, results, fields) => {
+        error && res.send(error.message)
+        return res.send(results);
       });
 });
 
